1374295e583c58b4e27bbcd6494c036e0cd13c3b
Benjamin Parker <ben@benparker.co.uk>
2015-04-17 15:49:38 +0100

    Nicer output from app Cleanup

--- src/main/scala/mrz/MRZApp.scala
+++ src/main/scala/mrz/MRZApp.scala
@@ -7,5 +7,16 @@ object MRZApp extends App with MRZReader {
 
   val mrz = readMRZ(filename)
 
-  println(mrz)
+  println("==================================================")
+  println("Issuing country: " + mrz.issuingCountry)
+  println("Last name: " + mrz.lastName)
+  println("Given names: " + mrz.givenNames)
+  println("Gender: " + mrz.gender)
+  println("Nationality: " + mrz.nationality)
+  println("Date of birth: " + mrz.dob)
+  println("Passport No: " + mrz.passportNo)
+  println("Passport expiry date: " + mrz.passportExpirationDate)
+  println("Personal number: " + mrz.personalNo)
+  println("==================================================")
+
 }
--- src/main/scala/mrz/MRZReader.scala
+++ src/main/scala/mrz/MRZReader.scala
@@ -16,9 +16,7 @@ trait MRZReader {
 
   private def parseImageTextCommandLine(filename: String) = {
     import scala.sys.process._
-    val command = s"tesseract $filename out"
-    println(s"Calling $command")
-    command.!
+    s"tesseract $filename out".!
     Source.fromFile("out.txt").mkString
   }
 
@@ -26,17 +24,4 @@ trait MRZReader {
     val startPos = ocrString.indexOf("P<")
     ocrString.substring(startPos, startPos + 89).replaceAll(System.getProperty("line.separator"), "")
   }
-
-  // This java lib doesnt work very well so we call the command from the system
-  //
-  //  private def parseImageText(file: File) = {
-  //    val tesseract = Tesseract.getInstance()
-  //    try {
-  //      val text = tesseract.doOCR(file)
-  //      println(text)
-  //      Some(text)
-  //    } catch {
-  //      case e: Exception => None
-  //    }
-  //  }
 }
